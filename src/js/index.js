7cd8680d3cbf3ce533a6b8360b1ccee71a831acc
Ross Angus <ross.angus@amazerealise.com>
2020-02-17 14:09:26 +0000

    Task 2: The list of tasks now being output to the page.

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,18 @@
-var url = 'http://localhost:4000/api/task';
+var url          = 'http://localhost:4000/api/task',
+	taskList     = document.querySelector('[data-js="tasklist"]'), // A DIV in the mark-up
+	taskFragment = document.createElement('ul');
 
 // Note that this will not work in any version of IE.
 fetch(url).then(function (response) {
-	console.log(response);
+	return response.json();
+}).then(function (json) {
+	for (var i = 0; i < json.length; i++) {
+		var task = document.createElement('li');
+		task.innerHTML = json[i].title;
+		taskFragment.appendChild(task);
+	}
+	// This way, the DOM is only updated once.
+	taskList.appendChild(taskFragment);
+}).catch(function (err) {
+	console.log('Fetch problem: ' + err.message);
 });
